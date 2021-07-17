dd61b0d02779494ec2d9f373f3337e432977291c
Bapic <43567337+Bapic@users.noreply.github.com>
2021-07-17 14:07:21 +0530

    updating prereq script

---
+++ Prereq/prereqs.ps1
@@ -0,0 +1,115 @@
+﻿Write-Output "Create deployment path"
+$tempDir = "C:\Temp"
+New-Item -Path $tempDir -ItemType directory -Force
+
+Start-Transcript "C:\Temp\Bootstrap.log"
+
+$ErrorActionPreference = 'SilentlyContinue'
+
+# Uninstall Internet Explorer
+Disable-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 -Online -NoRestart
+
+# Disabling IE Enhanced Security Configuration
+Write-Host "Disabling IE Enhanced Security Configuration"
+function Disable-ieESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+Disable-ieESC
+
+# Extending C:\ partition to the maximum size
+Write-Host "Extending C:\ partition to the maximum size"
+Resize-Partition -DriveLetter C -Size $(Get-PartitionSupportedSize -DriveLetter C).SizeMax
+
+
+# Installing tools
+
+Invoke-WebRequest -Uri https://github.com/Azure/azure-cli/releases/download/azure-cli-2.25.0/azure-cli-2.25.0.msi -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+# Pindown az cli version to avoid current Azure Arc-enabled app services version compatibility issues
+
+
+
+workflow ClientTools_01
+        {
+            $chocolateyAppList = 'az.powershell,kubernetes-cli,vcredist140,microsoft-edge,kubernetes-helm,vscode'
+            #Run commands in parallel.
+            # add this to the list for additional tools - azure-functions-core-tools-3,azurefunctions-vscode,dotnetcore-sdk,vscode-csharp,microsoftazurestorageexplorer,azcopy10,vscode,putty.install,microsoft-windows-terminal
+
+            Parallel 
+                {
+                    InlineScript {
+                        param (
+                            [string]$chocolateyAppList
+                        )
+                        if ([string]::IsNullOrWhiteSpace($using:chocolateyAppList) -eq $false)
+                        {
+                            try{
+                                choco config get cacheLocation
+                            }catch{
+                                Write-Output "Chocolatey not detected, trying to install now"
+                                iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+                            }
+                        }
+                        if ([string]::IsNullOrWhiteSpace($using:chocolateyAppList) -eq $false){   
+                            Write-Host "Chocolatey Apps Specified"  
+                            
+                            $appsToInstall = $using:chocolateyAppList -split "," | foreach { "$($_.Trim())" }
+                        
+                            foreach ($app in $appsToInstall)
+                            {
+                                Write-Host "Installing $app"
+                                & choco install $app /y -Force| Write-Output
+                            }
+                        }                        
+                    }
+                    Invoke-WebRequest "https://azuredatastudio-update.azurewebsites.net/latest/win32-x64-archive/stable" -OutFile "C:\Temp\azuredatastudio.zip"
+                    Invoke-WebRequest "https://aka.ms/azdata-msi" -OutFile "C:\Temp\AZDataCLI.msi"
+
+
+        }
+    }
+
+ClientTools_01 | Format-Table
+
+workflow ClientTools_02
+        {
+            #Run commands in parallel.
+            Parallel
+            {
+                InlineScript {
+                    Expand-Archive C:\Temp\azuredatastudio.zip -DestinationPath 'C:\Program Files\Azure Data Studio'
+                    Start-Process msiexec.exe -Wait -ArgumentList '/I C:\Temp\AZDataCLI.msi /quiet'
+                }
+            }
+        }
+        
+ClientTools_02 | Format-Table 
+
+New-Item -path alias:kubectl -value 'C:\ProgramData\chocolatey\lib\kubernetes-cli\tools\kubernetes\client\bin\kubectl.exe'
+New-Item -path alias:azdata -value 'C:\Program Files (x86)\Microsoft SDKs\Azdata\CLI\wbin\azdata.cmd'
+
+
+Write-Host "Installing Azure Data Studio Extensions"
+Write-Host "`n"
+
+$env:argument1="--install-extension"
+$env:argument2="Microsoft.arc"
+$env:argument3="microsoft.azuredatastudio-postgresql"
+
+& "C:\Program Files\Azure Data Studio\bin\azuredatastudio.cmd" $env:argument1 $env:argument2
+& "C:\Program Files\Azure Data Studio\bin\azuredatastudio.cmd" $env:argument1 $env:argument3
+
+Write-Host "Creating Azure Data Studio Desktop shortcut"
+Write-Host "`n"
+$TargetFile = "C:\Program Files\Azure Data Studio\azuredatastudio.exe"
+$ShortcutFile = "C:\Users\$env:adminUsername\Desktop\Azure Data Studio.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.TargetPath = $TargetFile
+$Shortcut.Save()
+
+
